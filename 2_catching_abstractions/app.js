94ceb37ada0226191607f75c41b67d21fc710835
Antoinette Smith <antoinette@primeacademy.io>
2016-08-05 09:01:49 -0500

    added more comments to code. added a strategy.

--- 2_catching_abstractions/app.js
+++ 2_catching_abstractions/app.js
@@ -2,6 +2,12 @@
  * This following code needs some abstraction.
  * It's pretty hard to follow what is going on...
  * You're our only hope!
+ *
+ * STRATEGY
+ * - If code is repeated, make that logic into a reusable function
+ * - Create functions that modify a single entity/object at a time
+ * - Wherever possible, try not to rely on global variables
+ * - Run your code after every change to make sure it still works
  */
 
 function Character(name, luck, isVillain) {
@@ -33,17 +39,18 @@ console.log('The villains at the start', capitol);
 
 var matches = [];
 
-var heroWins = 0;
-var capitolWins = 0;
+var heroWins = 0; // number of times a hero has won a match
+var capitolWins = 0; // number of times a capitol villain has won a match
 
 // create 10 matches
+// match a random hero with a random capitol villain
 for (var i = 0; i < 10; i++) {
   var match = [];
-  hero = randomWholeNumber(0, heroes.length - 1);
-  console.log('hero selected', hero);
+
+  var hero = randomWholeNumber(0, heroes.length - 1);
   match.push(heroes[hero]);
 
-  villain = randomWholeNumber(0, capitol.length - 1);
+  var villain = randomWholeNumber(0, capitol.length - 1);
   match.push(capitol[villain]);
 
   matches.push(match);
@@ -51,57 +58,57 @@ for (var i = 0; i < 10; i++) {
 
 console.log('Our scheduled matches', matches);
 
+// determine the winner and loser for each match
 matches.forEach(function (element) {
 
-  if (element[0].isAlive && element[1].isAlive) {
-    if (element[0].luck > element[1].luck) {
-      heroWins++;
+  if (element[0].isAlive && element[1].isAlive) { // if both contestants alive, run the match
+    if (element[0].luck > element[1].luck) { // if hero luck greater than villain luck
+      heroWins++; // add a win for the heroes
 
-      element[0].luck++;
-      if (element[0].luck > 10) {
+      element[0].luck++; // increase the hero's luck
+      if (element[0].luck > 10) { // winner's luck cannot be above 10
         element[0].luck = 10;
       }
 
-      element[1].luck--;
-      if (element[1].luck < 0) {
+      element[1].luck--; // decrease the villain's luck
+      if (element[1].luck < 0) { // if the luck falls below zero, character is dead
         element[1].isAlive = false;
       }
-    } else if (element[0].luck < element[1].luck) {
-      capitolWins++;
+    } else if (element[0].luck < element[1].luck) { // if hero luck less than villain luck, villain wins
+      capitolWins++; // add a win for the capitol/villains
 
-      element[1].luck++;
-      if (element[1].luck > 10) {
+      element[1].luck++; // increase the villain's luck
+      if (element[1].luck > 10) { // winner's luck cannot be above 10
         element[1].luck = 10;
       }
 
-      element[0].luck--;
-      if (element[0].luck < 0) {
+      element[0].luck--; // decrease the hero's luck
+      if (element[0].luck < 0) { // if the luck falls below zero, character is dead
         element[0].isAlive = false;
       }
     }
-  } else if (!element[1].isAlive && element[0].isAlive) {
-    heroWins++;
+  } else if (!element[1].isAlive && element[0].isAlive) { // if the villain is not alive, but the hero is, automatic win for hero
+    heroWins++; // add a win for the heroes
 
-    element[0].luck++;
-    if (element[0].luck > 10) {
+    element[0].luck++; // increase the hero's luck
+    if (element[0].luck > 10) { // winner's luck cannot be above 10
       element[0].luck = 10;
     }
 
-    element[1].luck--;
-    if (element[1].luck < 0) {
+    element[1].luck--; // decrease the villain's luck
+    if (element[1].luck < 0) { // if the luck falls below zero, character is dead
       element[1].isAlive = false;
     }
-  } else if (!element[0].isAlive && element[1].isAlive) {
-    capitolWins++;
+  } else if (!element[0].isAlive && element[1].isAlive) { // if the hero is not alive, but the villain is, automatic win for villain
+    capitolWins++; // add a win for the capitol/villains
 
-    element[1].luck++;
-
-    if (element[1].luck > 10) {
+    element[1].luck++; // increase the villain's luck
+    if (element[1].luck > 10) { // winner's luck cannot be above 10
       element[1].luck = 10;
     }
 
-    element[0].luck--;
-    if (element[0].luck < 0) {
+    element[0].luck--; // decrease the hero's luck
+    if (element[0].luck < 0) { // if the luck falls below zero, character is dead
       element[0].isAlive = false;
     }
   }
@@ -109,6 +116,7 @@ matches.forEach(function (element) {
 
 conclusion();
 
+// display the results of our matches in the console
 function conclusion() {
   console.log('Heroes at conclusion', heroes);
   console.log('Villains at conclusion', capitol);
