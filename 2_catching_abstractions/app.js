5cb6af291ae067288b42ae7b6ec5c30d5d46ef44
twinklefingers <B1-66-ER@Rays-MacBook-Pro.local>
2016-08-09 13:47:53 -0500

    Made it better

--- 2_catching_abstractions/app.js
+++ 2_catching_abstractions/app.js
@@ -79,69 +79,104 @@ console.log('Our scheduled matches', matches);
 
 matches.forEach(function(element) {
 
-            if (element[i].isAlive) { // if both contestants alive, run the match
+    // if (element[i].isAlive) { // if both contestants alive, run the match
 
-                //wins
-                function win(element) {
-                    element[i].luck++;
-                    if (element[i] == hero) {
-                        heroWins++;
-                    } else {
-                        capitolWins++;
-                    }
-                }
-
-
-
-
-
-
-                if (element[0].luck > element[1].luck) { // if hero luck greater than villain luck
-                    heroWins++; // add a win for the heroes
-                    element[0].luck++; // increase the hero's luck
-                } else if (element[0].luck < element[1].luck) { // if hero luck less than villain luck, villain wins
-                    capitolWins++; // add a win for the capitol/villains
-                    element[1].luck++; // increase the villain's luck
-
-                    //loses
-                    element[1].luck--; // decrease the villain's luck
-                    if (element[1].luck < 0) { // if the luck falls below zero, character is dead
-                        element[1].isAlive = false;
-                    }
-                    element[0].luck--; // decrease the hero's luck
-                    if (element[0].luck < 0) { // if the luck falls below zero, character is dead
-                        element[0].isAlive = false;
-                    }
-
-                    //etc
-                    if (element[0].luck > 10) { // winner's luck cannot be above 10
-                        element[0].luck = 10;
-                    }
-                    if (element[1].luck > 10) { // winner's luck cannot be above 10
-                        element[1].luck = 10;
-                    }
-
-                } else if (!element[1].isAlive && element[0].isAlive) { // if the villain is not alive, but the hero is, automatic win for hero
-                    heroWins++; // add a win for the heroes
-                } else if (!element[0].isAlive && element[1].isAlive) { // if the hero is not alive, but the villain is, automatic win for villain
-                    capitolWins++; // add a win for the capitol/villains
-                }
-            });
-
-        conclusion();
-
-        // display the results of our matches in the console
-        function conclusion() {
-            console.log('Heroes at conclusion', heroes);
-            console.log('Villains at conclusion', capitol);
-
-            if (capitolWins > heroWins) {
-                console.log('Not really the ending we were hoping for');
-            } else {
-                console.log('We did it!');
-            }
+    //wins
+    function win(element) {
+        element.luck++;
+        if (!element.isVillain) {
+            heroWins++;
+        } else {
+            capitolWins++;
         }
+    }
 
-        function randomWholeNumber(min, max) {
-            return Math.floor(Math.random() * (max - min) + min);
+
+
+
+
+    //
+    // if (element[0].luck > element[1].luck) { // if hero luck greater than villain luck
+    //     heroWins++; // add a win for the heroes
+    //     element[0].luck++; // increase the hero's luck
+    // } else if (element[0].luck < element[1].luck) { // if hero luck less than villain luck, villain wins
+    //     capitolWins++; // add a win for the capitol/villains
+    //     element[1].luck++; // increase the villain's luck
+
+    //loses
+    function lose(element) {
+        element.luck--;
+        if (element.isVillain) {
+            heroWins--;
+        } else {
+            capitolWins--;
         }
+    }
+
+
+
+
+    // element[1].luck--; // decrease the villain's luck
+    // if (element[1].luck < 0) { // if the luck falls below zero, character is dead
+    //     element[1].isAlive = false;
+    // }
+    // element[0].luck--; // decrease the hero's luck
+    // if (element[0].luck < 0) { // if the luck falls below zero, character is dead
+    //     element[0].isAlive = false;
+    // }
+
+    //etc
+    function luckBattle(element) {
+        if (element[1].luck > element[0].luck) {
+            win(element[1]);
+            lose(element[0]);
+        } else {
+            win(element[0]);
+            lose(element[1]);
+        }
+        if (element.luck > 10) { // winner's luck cannot be above 10
+            element.luck = 10;
+        }
+        if (element.luck <= 0) {
+            element.isAlive = false;
+        }
+    }
+
+
+
+
+    //         if (element[0].luck > 10) { // winner's luck cannot be above 10
+    //             element[0].luck = 10;
+    //         }
+    //         if (element[1].luck > 10) { // winner's luck cannot be above 10
+    //             element[1].luck = 10;
+    //         }
+    //
+    //     } else if (!element[1].isAlive && element[0].isAlive) { // if the villain is not alive, but the hero is, automatic win for hero
+    //         heroWins++; // add a win for the heroes
+    //     } else if (!element[0].isAlive && element[1].isAlive) { // if the hero is not alive, but the villain is, automatic win for villain
+    //         capitolWins++; // add a win for the capitol/villains
+    //     }
+    luckBattle(matches);
+    console.log(heroWins);
+    console.log(capitolWins);
+
+});
+
+conclusion();
+
+// display the results of our matches in the console
+function conclusion() {
+    console.log('Heroes at conclusion', heroes);
+    console.log('Villains at conclusion', capitol);
+
+    if (capitolWins > heroWins) {
+        console.log('Not really the ending we were hoping for');
+    } else {
+        console.log('We did it!');
+    }
+}
+
+function randomWholeNumber(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
